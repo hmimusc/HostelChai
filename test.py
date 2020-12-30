ebbcccec64bd15e6ec9f6a7b24ad40023aafd611
HM Imu SC <76000211+hmimusc@users.noreply.github.com>
2020-12-30 11:39:23 +0600

    HM Imu SC

--- test.py
+++ test.py
@@ -3,6 +3,15 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HostelChai.settings')
 import django
 django.setup()
 
+from django.db import connection
+from django.conf import settings
+from PIL import Image
 
+cursor = connection.cursor()
 
-print('test')
+command = f'select hostel_id from hostel'
+cursor.execute(command)
+
+hostel_ids = [id[0] for id in list(cursor.fetchall())]
+
+print(hostel_ids)
